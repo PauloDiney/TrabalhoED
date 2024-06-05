c0603fd1146134336ad6fe31cd8b1023eccbf81c
PauloDiney <paulo19martin@gmail.com>
2024-06-05 14:34:29 -0300

    Arrumado Parte 1

---
+++ JS/codigosChamar.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const selectElement = document.querySelector('.form-select');
+    const codigosSections = document.querySelectorAll('.codigos-explicao');
+
+    selectElement.addEventListener('change', function () {
+        const selectedOption = selectElement.value;
+        
+        // Oculta todas as seções de códigos
+        codigosSections.forEach(section => {
+            section.style.display = 'none';
+        });
+        
+        // Mostra a seção correspondente à opção selecionada
+        const selectedSection = document.getElementById(selectedOption);
+        if (selectedSection) {
+            selectedSection.style.display = 'block';
+        }
+    });
+});
