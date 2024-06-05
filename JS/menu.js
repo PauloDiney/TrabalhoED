8fe6fcc96b9667bfa6cc1e95227e9465e25db3a0
PauloDiney <paulo19martin@gmail.com>
2024-06-04 23:05:12 -0300

    Front-menu

---
+++ JS/menu.js
@@ -0,0 +1,30 @@
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
+
+//expandir menu
+
+ var btnexp = document.querySelector('#btn-exp')
+ var menuSide = document.querySelector('.menu-lateral')
+
+btnexp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
+
+
+
+
+
+
+
+
